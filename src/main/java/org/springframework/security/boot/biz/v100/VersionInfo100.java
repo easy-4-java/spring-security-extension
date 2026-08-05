85bc49db35ae6391dec5aea660b92020344cb7f8
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 00:55:19 +0800

    feat(1.0.x): 添加 feature/1.0.x 特有的 VersionInfo 类

---
+++ src/main/java/org/springframework/security/boot/biz/v100/VersionInfo100.java
@@ -0,0 +1,17 @@
+package org.springframework.security.boot.biz.v100;
+
+/**
+ * Version identifier for spring-security-extension feature/1.0.x
+ * This class is unique to feature/1.0.x branch
+ */
+public final class VersionInfo100 {
+    
+    public static final String VERSION = "1.0.x.20260630-SNAPSHOT";
+    public static final String BRANCH = "feature/1.0.x";
+    public static final String BUILD_TIMESTAMP = "2026-08-06";
+    public static final String JAVA_TARGET = "1.8";
+    public static final String SERVLET_API = "javax.servlet";
+    
+    private VersionInfo100() {
+    }
+}
