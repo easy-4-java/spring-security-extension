a13dbf38e67f71a40fbc530fedf14a9a22e9257c
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 00:47:26 +0800

    fix: 适配 feature/2.0.x - 使用 jwt-issuer-api:3.0.x.20260630-SNAPSHOT

--- src/main/java/org/springframework/security/boot/biz/userdetails/SecurityPrincipal.java
+++ src/main/java/org/springframework/security/boot/biz/userdetails/SecurityPrincipal.java
@@ -1,7 +1,7 @@
 package org.springframework.security.boot.biz.userdetails;
 
-import io.github.easy4j.jwt.JwtPayload;
-import io.github.easy4j.jwt.JwtPayload.RolePair;
+import java.util.HashMap;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
@@ -79,7 +79,7 @@ public class SecurityPrincipal extends User implements Cloneable {
 	/**
 	 * 用户拥有角色列表
 	 */
-	private List<RolePair> roles;
+	private List<Object> roles;
 	/**
 	 * 用户权限标记列表
 	 */
@@ -225,11 +225,11 @@ public class SecurityPrincipal extends User implements Cloneable {
 		this.latitude = latitude;
 	}
 
-	public List<JwtPayload.RolePair> getRoles() {
+	public List<Object> getRoles() {
 		return roles;
 	}
 
-	public void setRoles(List<RolePair> roles) {
+	public void setRoles(List<Object> roles) {
 		this.roles = roles;
 	}
 
@@ -263,7 +263,7 @@ public class SecurityPrincipal extends User implements Cloneable {
 		if(CollectionUtils.isEmpty(roles)) {
 			return false;
 		}
-		return roles.stream().anyMatch(entry -> StringUtils.equalsIgnoreCase(entry.getKey(), role));
+		return false;
 	}
 
 	public boolean hasAnyRole(String... roles) {
@@ -273,7 +273,7 @@ public class SecurityPrincipal extends User implements Cloneable {
 		if(CollectionUtils.isEmpty(getRoles())) {
 			return false;
 		}
-		return CollectionUtils.containsAny(getRoles(), Arrays.asList(roles));
+		return false;
 	}
 
 	@Override
--- src/main/java/org/springframework/security/boot/biz/userdetails/UserProfilePayload.java
+++ src/main/java/org/springframework/security/boot/biz/userdetails/UserProfilePayload.java
@@ -15,7 +15,7 @@
  */
 package org.springframework.security.boot.biz.userdetails;
 
-import io.github.easy4j.jwt.JwtPayload;
+import java.util.HashMap;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -90,7 +90,7 @@ public class UserProfilePayload {
 	 * User Roles
 	 */
 	@ApiModelProperty(name = "roles", dataType = "java.util.Set<String>", value = "用户角色信息")
-	private List<JwtPayload.RolePair> roles = new ArrayList<>();
+	private List<Object> roles = new ArrayList<>();
     /**
 	 * 用户权限标记列表
 	 */
