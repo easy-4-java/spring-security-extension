1aebd1d484213cc3a8bda573a5859e3fffaf2819
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 00:20:26 +0800

    fix: 重写 CustomWebSecurityExpressionRoot 不使用 spring-biz

--- src/main/java/org/springframework/security/boot/biz/CustomWebSecurityExpressionRoot.java
+++ src/main/java/org/springframework/security/boot/biz/CustomWebSecurityExpressionRoot.java
@@ -1,6 +1,5 @@
 package org.springframework.security.boot.biz;
 
-import org.springframework.biz.utils.WebUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.WebSecurityExpressionRoot;
@@ -11,8 +10,6 @@ import java.util.Objects;
 
 public class CustomWebSecurityExpressionRoot  extends WebSecurityExpressionRoot {
 
-    // private FilterInvocation filterInvocation;
-    /** Allows direct access to the request object */
     public final HttpServletRequest request;
 
     public CustomWebSecurityExpressionRoot(Authentication a, FilterInvocation fi) {
@@ -20,17 +17,12 @@ public class CustomWebSecurityExpressionRoot  extends WebSecurityExpressionRoot 
         this.request = fi.getRequest();
     }
 
-    /**
-     * Takes a specific IP address or a range using the IP/Netmask (e.g. 192.168.1.0/24 or
-     * 202.24.0.0/14).
-     *
-     * @param ipAddress the address or range of addresses from which the request must
-     * come.
-     * @return true if the IP address of the current request is in the required range.
-     */
     @Override
     public boolean hasIpAddress(String ipAddress) {
-        String remoteAddr = Objects.toString(WebUtils.getRemoteAddr(request) , request.getRemoteAddr());
+        String remoteAddr = request.getRemoteAddr();
+        if (remoteAddr == null) {
+            remoteAddr = "";
+        }
         return (new IpAddressMatcher(ipAddress).matches(remoteAddr));
     }
 
